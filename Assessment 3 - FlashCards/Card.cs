d73499d74f02d9398a60c6168785e13f70ca858f
Preetmohan Manchanda <preetmohansm@outlook.com>
2021-03-15 12:21:28 +0700

    Add project files.

---
+++ Assessment 3 - FlashCards/Card.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_3___FlashCards
+{
+    class Card
+    {
+        private string Word;
+        private string Def;
+        private bool Flipped;
+
+        public Card(String Word, string Def)
+        {
+            this.Word = Word;
+            this.Def = Def;
+            Flipped = false;
+        }
+
+        public string GetCard()
+        {
+            if (Flipped == false)
+            {
+                return Word;
+            }
+            else 
+            {
+                return Def;
+            }
+        }
+
+        public void Flip()
+        {
+            if (Flipped == true)
+            {
+                Flipped = false;
+            }
+            else
+            {
+                Flipped = true;
+            }
+        }
+    }
+
+}
---
+++ Assessment 3 - FlashCards/Deck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_3___FlashCards
+{
+    class Deck
+    {
+        private string FileName;
+        private Card[] Cards;
+
+        public Deck(string FileName)
+        {
+            Cards = new Card[1000];
+        }
+
+        public void ShuffleCards()
+        {
+
+        }
+    }
+}
